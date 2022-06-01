1bcc283341b4a6759588052a578ce48134831fd9
Sunny gupta <82308715+Sunny98198@users.noreply.github.com>
2022-06-01 17:13:08 +0530

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,15 @@
+from typing import cast
+
+
+animal=['ant','dog','cat','bear']
+
+sort_animal=sorted(animal)
+
+print('animal list {}:'.format(animal))
+
+print('after sorting animal list {}:'.format(sort_animal)  )
+
+
+animal.sort()
+
+print('after sorted list {}'.format(animal))
